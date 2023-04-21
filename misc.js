69b40db2cd2d5f726263e1dd856f19f8a04054ca
Garrett Pace <elderpace6@gmail.com>
2023-04-20 20:17:55 -0600

    progress!

--- misc.js
+++ misc.js
@@ -32,7 +32,6 @@ function hasYellow(marbleArr) {
 
 let result1 = hasYellow(['yellow', 'red', 'blue', 'yellow', 'green'])
 let result2 = hasYellow(['red', 'red', 'red', 'red', 'red'])
-let result2 = hasYellow(['red', 'red', 'red', 'red', 'red'])
 
 console.log(result1)
 console.log(result2)
@@ -42,54 +41,42 @@ console.log(result2)
 // Does my function actually do what it was supposed to do? Why or why not?
 // Answer these question in comments below. Please feel free to run your
 // own tests with the function.
-// No, result1 seems to work, but result2 does not
+
+// No; the function is only returning a true/false value for i at index 0, because the 'return false' is inside the for-loop. To get it to run through each bag of marbles until it either reaches a yellow and returns true or reaches the end of the array and returns false, the return false needs to be moved outside the for-loop:
+
+// function hasYellow(marbleArr) {
+//     for (let i = 0; i < marbleArr.length; i++) {
+//         console.log(i)
+//         if (marbleArr[i] === 'yellow') {
+//             return true
+//         } 
+//     }
+//     return false
+// }
 
 
 
 // *** Problem 2 ***
-
-//  Students should be able to work through intricate or complicated
-//  instructions.
+//  Students should be able to work through intricate or complicated instructions.
 
 // 2.1
 //  As practice in following difficult instructions, do the following:
-
-//  Write a function called 'changeNums'
-//  that takes in three arguments: 'array',
-//  'operator', and 'amount'.
-
-//  The 'array' argument will be an array of
-//  numbers.
-
-//  The operator argument will be a string
-//  that can be one of four things: 'add',
-//  'subtract', 'multiply', or 'divide'.
-
-//  The number argument will be a number
-//  that you want to operate against each
-//  number in the array argument.
-
-//  Inside the function, you will create
-//  a new array that is modified (mapped) from
-//  the original array passed in as an argument.
-//  Use the array.map method to execute this
-//  transformation.
-
-//  In the map method's callback, do the mathmatical
-//  operation to the array element (a number)
-//  with the number that was the third argument passed into
-//  the outer function.
-
+//  Write a function called 'changeNums' that takes in three arguments: 'array','operator', and 'amount'.
+//  The 'array' argument will be an array of numbers.
+//  The operator argument will be a string that can be one of four things: 'add', 'subtract', 'multiply', or 'divide'.
+//  The number argument will be a number that you want to operate against each number in the array argument.
+//  Inside the function, you will create a new array that is modified (mapped) from the original array passed in as an argument. Use the array.map method to execute this transformation.
+//  In the map method's callback, do the mathmatical operation to the array element (a number) with the number that was the third argument passed into the outer function.
 //  Return the mapped array from the entire function
 
-
+function changeNums(array, operator, amount)
+let array = [0,1,2,3,4,5]
+let operator = 'divide'
+let amount = 5
+array.map
 
 // 2.2
-//  Invoke the 'changeNums' function passing
-//  in a valid value for each paramter. Save the
-//  result to a variable.
-
-//  You'll have to log to the console to see
-//  the returned array.
+//  Invoke the 'changeNums' function passing in a valid value for each parameter. Save the result to a variable.
+//  You'll have to log to the console to see the returned array.
 
 
